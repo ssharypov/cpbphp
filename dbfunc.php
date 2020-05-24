6519a409f0facc1cd6ccd6b9746a99fec7450855
Шарыпов Саян Доржеевич <ssharypov@vegosm.local>
2020-05-24 10:19:54 +0800

    поправил проверку кол-ва подразделений для корректного добавления

--- dbfunc.php
+++ dbfunc.php
@@ -1,10 +1,9 @@
 <?php
 function add_dep($depname,$depsort) {
     global $dblink;
-    $query = "SELECT COUNT(id) FROM departments";
+    $query = "SELECT COUNT(depid) FROM departments";
     $result = mysqli_query($dblink,$query);
-    $row = mysqli_fetch_all($result);
-    echo var_dump($raw);
+    $row = mysqli_fetch_row($result);
     if($row[0] > 1) {
         $query = "UPDATE departments SET depsort=depsort+1 WHERE depsort > $depsort";
     } else {
